cac0849fd36ad1d4a7e1385f6d5086eb413e22bc
usman-97 <usmanshabir65@hotmail.com>
2021-05-05 05:34:07 +0100

    update

--- viewItem.php
+++ viewItem.php
@@ -36,10 +36,6 @@ $view->lotStatus = '';
 
 require_once ('searchBar.php'); // Lot Search script
 
-// $view->auctionDate = date($view->getItem[0]->getEndDatetime());
-// var_dump($view->auctionDate == $view->currentDate);
-// var_dump($view->currentDate > $view->getItem[0]->getDatetime());
-
 // If auction has been started and then
 // Inform user according to that
 if ($view->currentDate <= $view->getItem[0]->getDatetime())
@@ -74,73 +70,5 @@ if (!$view->bidItemData)
     // Then display a message to inform user
     $view->emptyBidError = 'No bid has been placed on this item yet.';
 }
-
-// If placeBid button is pressed
-//if (isset($_POST['placeBid']))
-//{
-//    // If user bid is not empty
-//    if (empty(trim($_POST['userBid'])))
-//    {
-//        $view->userBidError = 'Insufficient amount';
-//    }
-//    else
-//    {
-//        // Check if user typed bid is numeric
-//        if (is_numeric($_POST['userBid']))
-//        {
-//            // Get the highest bid for item
-//            $highestBid = $view->bidItemDataSet->checkHighestBid($_POST['lotID'], $_POST['userBid']);
-//
-//            // Check If user's bid is greater than opening item bid
-//            if ($_POST['lotPrice'] < $_POST['userBid']) {
-//                // Check if user's bid is greater than current highest bid
-//                if ($highestBid == true) {
-//                    $userBid = $_POST['userBid'];
-//                    // Place user bid
-//                    $view->bidItemDataSet->placeBid(intval($_SESSION['userID']), intval($_POST['lotID']), intval($_POST['auctionID']), $userBid);
-//                    /*if ($bidItemDataSet->checkUserLotBid($_POST['lotID'], $_SESSION['userID']))
-//                    {
-//                        $bidItemDataSet->updateUserBid($_POST['lotID'], $_SESSION['userID'], $userBid);
-//                    }
-//                    else
-//                    {
-//                        $bidItemDataSet->placeBid(intval($_SESSION['userID']), intval($_POST['lotID']), intval($_POST['auctionID']), $userBid);
-//                    }*/
-//                    $userBid = '';
-//                    $view->userBidError = '';
-//                    // header("location: viewItem.php");
-//                    // unset($_SESSION['viewItem']);
-//                    // echo 'You have successfully placed a bid!';
-//                } else {
-//                    $view->userBidError = 'Your bid is lower than the highest bid for this item.';
-//                }
-//            }
-//            else
-//            {
-//                $view->userBidError = 'Your bid is lower than the opening bid for this item.';
-//            }
-//        }
-//        else
-//        {
-//            $view->userBidError = 'Invalid amount';
-//        }
-//    }
-//}
-//else
-//{
-//    $view->userBidError = '';
-//}
-
-//if (isset($_POST['bidBtn']))
-//{
-//    // $_SESSION['userCurrentBid'] = $_POST['userBid'];
-//    $_POST['userBidStore'] = $_POST['userBid'];
-//}
-//var_dump($_SESSION['userCurrentBid']);
-
-/*if (isset($_POST['back']))
-{
-    header("location: auctionItem.php");
-}*/
 
 require_once ('Views/viewItem.phtml');
