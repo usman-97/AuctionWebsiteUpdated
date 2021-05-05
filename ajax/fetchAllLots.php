db1a45c0ae29094eab4117f14f6a7effc52a55a6
usman-97 <usmanshabir65@hotmail.com>
2021-05-05 01:16:35 +0100

    update

--- ajax/fetchAllLots.php
+++ ajax/fetchAllLots.php
@@ -1,24 +1,28 @@
 <?php
 require_once ('../Models/AuctionItemDateSet.php');
 
-session_start();
-$auctionItem = new AuctionItemDateSet();
+session_start(); // start session
+$auctionItem = new AuctionItemDateSet(); // AuctionItemDataSet instance
 
-$q = $_REQUEST["q"];
-$r = $_REQUEST["r"];
-$s = $_REQUEST["s"];
-$t = $_REQUEST["t"];
+$q = $_REQUEST["q"]; // Sorting filter
+$r = $_REQUEST["r"]; // Selected category
+$s = $_REQUEST["s"]; // Minimum price
+$t = $_REQUEST["t"]; // Maximum price
 // var_dump($_SESSION['searchedItem']);
 
+// If user is using search
 if (isset($_SESSION['searchMode']))
 {
+    // then fetch items which matching with user's search with their chosen filters
     $auctionItemDataSet = $auctionItem->fetchSomeAuctionItem($_SESSION['searchedItem'], $_SESSION['lotPerPage'], $_SESSION['lotPerPage'] + 20 , $q, $r, $s, $t);
 }
 else
 {
+    // otherwise fetch all lots which user's chosen filters
     $auctionItemDataSet = $auctionItem->fetchAllAuctionItem($_SESSION['lotPerPage'], $_SESSION['lotPerPage'] + 20 , $q, $r, $s, $t);
 }
 
+// Start session for each chosen filter
 if ($q != '')
 {
     $_SESSION['sortingFilter'] = $q;
@@ -39,4 +43,4 @@ if ($t != '')
     $_SESSION['maxPrice'] = $t;
 }
 
-echo json_encode($auctionItemDataSet);
+echo json_encode($auctionItemDataSet); // Send requested data back to client side
